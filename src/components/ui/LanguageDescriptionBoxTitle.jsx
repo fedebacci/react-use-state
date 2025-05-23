6fcdf45d1e38d01a9abb8334cbe9d91eb8bae9f3
fedebacci <federicobacci1999@gmail.com>
2025-05-23 18:20:53 +0200

    sistemazione presentazione titolo

--- src/components/ui/LanguageDescriptionBoxTitle.jsx
+++ src/components/ui/LanguageDescriptionBoxTitle.jsx
@@ -1,6 +1,6 @@
 export default function LanguageDescriptionBoxTitle ({ title }) {
     return (
-        <h6 className="fw-bold fs-5">
+        title && <h6 className="fw-bold fs-5">
             {title}
         </h6>
     );
